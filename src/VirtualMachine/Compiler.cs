1d472ddb2d52c6e3a4a9b51451f965f78449b700
Dylan Turner <dylantdmt@gmail.com>
2020-09-08 22:17:22 -0400

    Added main subfunctions to compiler class

--- src/VirtualMachine/Compiler.cs
+++ src/VirtualMachine/Compiler.cs
@@ -1,6 +1,34 @@
+using System.Collections.Generic;
+
 namespace snakescript {
     class Compiler {
+        private static Function compileFunc(CompoundToken funcDef) {
+            return new Function(VmValueType.UndDef, VmValueType.UndDef, null);
+        }
+
+        private static OpCode[] compileStmt(CompoundToken stmt) {
+            return new OpCode[] {};
+        }
+
         public static (OpCode[], Function[]) Translate(CompoundToken ast) {
+            var opCodes = new List<OpCode>();
+            var funcs = new List<Function>();
+
+            var children = ast.Children;
+            foreach(var child in children) {
+                switch(child.Type) {
+                    case TokenType.FuncDef:
+                        funcs.Add(compileFunc(child as CompoundToken));
+                        break;
+                    
+                    case TokenType.Stmt:
+                        var stmtOpCodes = compileStmt(child as CompoundToken);
+                        foreach(var opCode in stmtOpCodes) {
+                            opCodes.Add(opCode);
+                        }
+                        break;
+                }
+            }
             return (new OpCode[] {}, new Function[] {});
         }
     }
